7f41b20c93579230460f0640a9f99d483a2d16da
James Hall <llahwj33@gmail.com>
2016-04-16 21:07:37 -0400

    Completed the main page of The Egg.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,14 +10,23 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     // Relative scroll position from top of window
     var scrollCheck = $(window).scrollTop();
+    var windowTop = $(window).height() * 1/2;
 
-    if(scrollCheck > $('.items-wrapper').offset().top - ($(window).height() * 1/2)) {
+    if(scrollCheck > $('.items-wrapper').offset().top - windowTop) {
       $('.items-display li').each(function(i) {
           setTimeout(function() {
             $('.items-display li').eq(i).addClass('slideFigures');
           }, 350 * (i + 1));
       })
      }
+
+     if(scrollCheck > $('.about-wrapper').offset().top - windowTop - 200) {
+       $('.about-wrapper').addClass('aboutUp');
+     }
+
+     $('.intro-display').css({
+       'transform' : 'translateY(0' + scrollCheck * 0.05 + '%)'
+     })
 
   }) // Window Scroll END
 
